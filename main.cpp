9243347ae94cfec94f06ecc9742db0758eff940f
GaProgMan <jamie@taylorj.org.uk>
2014-02-16 17:08:13 +0000

    Added methods for outputting an entire matrix to the console

--- main.cpp
+++ main.cpp
@@ -5,15 +5,51 @@
 
 using namespace std;
 
+// Used to output a single vector to the display.
+// If and x, y or z value is equal to 0, we show
+// a blank string in it's place
+void displayVector(Vector3d inVectorToDisplay)
+{
+    cout << setw(5);
+    if (inVectorToDisplay.xValue() > 0)
+        cout << inVectorToDisplay.xValue();
+    else
+        cout << ' ';
+    cout << setw(5);
+        if (inVectorToDisplay.yValue() > 0)
+        cout << inVectorToDisplay.yValue();
+    else
+        cout << ' ';
+    cout << setw(5);
+        cout << setw(5);
+        if (inVectorToDisplay.zValue() > 0)
+        cout << inVectorToDisplay.zValue();
+    else
+        cout << ' ';
+    cout << endl;
+}
+
+// Used to output an entire matrix to the display
+void displayMatrix(Matrix33d* inMatrixToDisplay)
+{
+    displayVector(inMatrixToDisplay->getRow1());
+    displayVector(inMatrixToDisplay->getRow2());
+    displayVector(inMatrixToDisplay->getRow3());
+}
+
 int main()
 {
-    Matrix33d* myMatrix = new Matrix33d(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    // Create a new instance of the matrix class
+    // for testing with.
+    Matrix33d* myMatrix = new Matrix33d(1, 0, 3, 4, 5, 6, 0, 8, 9);
 
     cout << "Matrix created" << endl;
 
-    cout << setw(5) << myMatrix->getRow1().xValue() <<  setw(5) << myMatrix->getRow1().yValue() <<  setw(5) << myMatrix->getRow1().zValue() << endl;
-    cout << setw(5) << myMatrix->getRow2().xValue() <<  setw(5) << myMatrix->getRow2().yValue() <<  setw(5) << myMatrix->getRow2().zValue() << endl;
-    cout << setw(5) << myMatrix->getRow3().xValue() <<  setw(5) << myMatrix->getRow3().yValue() <<  setw(5) << myMatrix->getRow3().zValue() << endl;
+//    cout << setw(5) << myMatrix->getRow1().xValue() <<  setw(5) << myMatrix->getRow1().yValue() <<  setw(5) << myMatrix->getRow1().zValue() << endl;
+//    cout << setw(5) << myMatrix->getRow2().xValue() <<  setw(5) << myMatrix->getRow2().yValue() <<  setw(5) << myMatrix->getRow2().zValue() << endl;
+//    cout << setw(5) << myMatrix->getRow3().xValue() <<  setw(5) << myMatrix->getRow3().yValue() <<  setw(5) << myMatrix->getRow3().zValue() << endl;
+
+    displayMatrix(myMatrix);
 
 //    cout << "Enter a value to check the matrix against: ";
 //
