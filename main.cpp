5a6a5a44cf280bcc4d5b8c0bac59c70fb0e5a0a6
GaProgMan <jamie@taylorj.org.uk>
2014-03-19 19:34:08 +0000

    Implemented Sudoku printing method that prints current state of board

--- main.cpp
+++ main.cpp
@@ -44,11 +44,164 @@ void displayMatrix(Matrix33d* inMatrixToDisplay)
     displayVector(inMatrixToDisplay->getRow3());
 }
 
+void displaySudokuTopThreeRows(Sudoku* inSudokuBoardToDisplay)
+{
+    // Top row, top left-most row
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowLeft()->getRow1().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowLeft()->getRow1().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowLeft()->getRow1().zValue();
+
+    // Top row, top centre row
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowCentre()->getRow1().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowCentre()->getRow1().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowCentre()->getRow1().zValue();
+
+    //Top row, top right-most row
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowRight()->getRow1().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowRight()->getRow1().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowRight()->getRow1().zValue();
+    cout << endl;
+
+    // Top row, left-most second row
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowLeft()->getRow2().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowLeft()->getRow2().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowLeft()->getRow2().zValue();
+
+    // Top row, centre second row
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowCentre()->getRow2().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowCentre()->getRow2().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowCentre()->getRow2().zValue();
+
+    //Top row, right-most second row
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowRight()->getRow2().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowRight()->getRow2().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowRight()->getRow2().zValue();
+    cout << endl;
+
+    // Top row, left-most final row
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowLeft()->getRow3().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowLeft()->getRow3().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowLeft()->getRow3().zValue();
+
+    // Top row, centre final row
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowCentre()->getRow3().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowCentre()->getRow3().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowCentre()->getRow3().zValue();
+
+    //Top row, right-most final row
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowRight()->getRow3().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowRight()->getRow3().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getTopRowRight()->getRow3().zValue();
+    cout << endl;
+}
+
+void displaySudokuCentreThreeRows(Sudoku* inSudokuBoardToDisplay)
+{
+    // Top row, top left-most row
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowLeft()->getRow1().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowLeft()->getRow1().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowLeft()->getRow1().zValue();
+
+    // Top row, top centre row
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowCentre()->getRow1().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowCentre()->getRow1().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowCentre()->getRow1().zValue();
+
+    //Top row, top right-most row
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowRight()->getRow1().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowRight()->getRow1().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowRight()->getRow1().zValue();
+    cout << endl;
+
+    // Top row, left-most second row
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowLeft()->getRow2().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowLeft()->getRow2().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowLeft()->getRow2().zValue();
+
+    // Top row, centre second row
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowCentre()->getRow2().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowCentre()->getRow2().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowCentre()->getRow2().zValue();
+
+    //Top row, right-most second row
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowRight()->getRow2().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowRight()->getRow2().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowRight()->getRow2().zValue();
+    cout << endl;
+
+    // Top row, left-most final row
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowLeft()->getRow3().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowLeft()->getRow3().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowLeft()->getRow3().zValue();
+
+    // Top row, centre final row
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowCentre()->getRow3().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowCentre()->getRow3().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowCentre()->getRow3().zValue();
+
+    //Top row, right-most final row
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowRight()->getRow3().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowRight()->getRow3().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getCentreRowRight()->getRow3().zValue();
+    cout << endl;
+}
+
+void displaySudokuBottomThreeRows(Sudoku* inSudokuBoardToDisplay)
+{
+    // Top row, top left-most row
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowLeft()->getRow1().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowLeft()->getRow1().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowLeft()->getRow1().zValue();
+
+    // Top row, top centre row
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowCentre()->getRow1().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowCentre()->getRow1().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowCentre()->getRow1().zValue();
+
+    //Top row, top right-most row
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowRight()->getRow1().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowRight()->getRow1().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowRight()->getRow1().zValue();
+    cout << endl;
+
+    // Top row, left-most second row
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowLeft()->getRow2().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowLeft()->getRow2().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowLeft()->getRow2().zValue();
+
+    // Top row, centre second row
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowCentre()->getRow2().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowCentre()->getRow2().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowCentre()->getRow2().zValue();
+
+    //Top row, right-most second row
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowRight()->getRow2().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowRight()->getRow2().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowRight()->getRow2().zValue();
+    cout << endl;
+
+    // Top row, left-most final row
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowLeft()->getRow3().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowLeft()->getRow3().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowLeft()->getRow3().zValue();
+
+    // Top row, centre final row
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowCentre()->getRow3().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowCentre()->getRow3().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowCentre()->getRow3().zValue();
+
+    //Top row, right-most final row
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowRight()->getRow3().xValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowRight()->getRow3().yValue();
+    cout << setw(5) << inSudokuBoardToDisplay->getBottomRowRight()->getRow3().zValue();
+    cout << endl;
+}
+
 void displaySudokuBoard(Sudoku* inSudokuBoardToDisplay)
 {
-    displayMatrix(inSudokuBoardToDisplay->getTopRowLeft());
-    displayMatrix(inSudokuBoardToDisplay->getTopRowCentre());
-    displayMatrix(inSudokuBoardToDisplay->getTopRowLeft());
+    displaySudokuTopThreeRows(inSudokuBoardToDisplay);
+    displaySudokuCentreThreeRows(inSudokuBoardToDisplay);
+    displaySudokuBottomThreeRows(inSudokuBoardToDisplay);
 }
 
 int main()
